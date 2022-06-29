fc773cbaf1474aa5ee9c6de6b77a71b453be68ab
Admin <hannguyen7796@gmail.com>
2022-06-29 16:39:39 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+class Customer {
+    constructor(id, fullName, email, phone, address, balance, isDeleted) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.balance = balance;
+        this.isDeleted = isDeleted;
+    }
+
+
+}
